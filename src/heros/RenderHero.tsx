0f6976aaa14c0808994df6f71f12474bd9225400
duphub <73028720+duphub42@users.noreply.github.com>
2026-02-14 22:50:51 +0100

    Update RenderHero.tsx

--- src/heros/RenderHero.tsx
+++ src/heros/RenderHero.tsx
@@ -1,33 +1,23 @@
 import React from 'react'
-
 import type { Page } from '@/payload-types'
-
 import { HighImpactHero } from '@/heros/HighImpact'
 import { LowImpactHero } from '@/heros/LowImpact'
-import { MediumImpactHero } from '@/heros/MediumImpact'
-import { PhilippBacherHero } from '@/heros/PhilippBacher' // <--- Unser neuer Import
+import { PhilippBacherHero } from '@/heros/philippBacher'  // <-- Kleinschreibung!
+
+const heroes = {
+  highImpact: HighImpactHero,
+  lowImpact: LowImpactHero,
+  philippBacher: PhilippBacherHero,
+}
 
 export const RenderHero: React.FC<Page['hero']> = (props) => {
   const { type } = props || {}
 
   if (!type || type === 'none') return null
 
-  if (type === 'highImpact') {
-    return <HighImpactHero {...props} />
-  }
+  const HeroToRender = heroes[type]
 
-  if (type === 'mediumImpact') {
-    return <MediumImpactHero {...props} />
-  }
+  if (!HeroToRender) return null
 
-  if (type === 'lowImpact') {
-    return <LowImpactHero {...props} />
-  }
-
-  // Hier ist die neue Weiche:
-  if (type === 'philippBacher') {
-    return <PhilippBacherHero {...props} />
-  }
-
-  return null
+  return <HeroToRender {...props} />
 }
